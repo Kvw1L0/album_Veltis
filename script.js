e186bc0ab4aca2f2a10c2071732ca4152e8db48e
Camilo Martinez <134472412+Kvw1L0@users.noreply.github.com>
2025-12-08 23:37:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // --- 1. Definir constantes y variables de estado ---
 const laminas = [
-  "Foto con mi súper equipo",
-  "haciendo una cara chistosa para el Grinch",
-  "Foto con el agente Z",
-  "Foto con un reno de Santa ",
-  "Un adulto en brazos del grupo"
+  "En mi traje de gala",
+  "Con mi líder favorito",
+  "Con un Reconocido 2025",
+  "Junto a la Mesa de la Noche ",
+  "En la pista de baile",
+  "Mi Gala en una Foto" ,
 ];
 
 let currentLamina = null;
